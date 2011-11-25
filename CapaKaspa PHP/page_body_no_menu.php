770fff8e7ca4bf90f3926a36be8c3faaf64ac819
Sebastien JOUVE <sjouve@gmail.com>
2011-11-25 23:30:07 +0100

    Déplacement recherche en haut.

--- CapaKaspa PHP/page_body_no_menu.php
+++ CapaKaspa PHP/page_body_no_menu.php
@@ -47,55 +47,43 @@ getMois($month);
   		<tr height="60">
 			<td width="430" height="60"><a href="http://www.capakaspa.info" style="display: block; height: 100%; width: 100%;">&nbsp;</a></td>
 			<td width="470" colspan="4">
-        	
-          <!--/* CapaKaspa Bandeau Haut */-->
-          
-			
+        		<h3>Rechercher dans tout CapaKaspa (dont Forum et Blog)</h3>
+	          	<form action="http://www.capakaspa.info/recherche.php" id="cse-search-box">
+				  <div>
+				  	
+				    <input type="hidden" name="cx" value="partner-pub-8069368543432674:7445098107" />
+				    <input type="hidden" name="cof" value="FORID:10" />
+				    <input type="hidden" name="ie" value="ISO-8859-1" />
+				    <input type="text" name="q" size="31" />
+				    <input type="submit" name="sa" value="Rechercher" />
+				  </div>
+				</form>
+				
+				<script type="text/javascript" src="http://www.google.fr/coop/cse/brand?form=cse-search-box&amp;lang=fr"></script>
+
         	</td>
 		</tr>
     	<tr height="20">
-			<td width="400"><h1><?
-			print "$jour[$day] ";
-			print Date(d)." ";
-			print "$mois[$month] ";
-			print Date(Y);?></h1></td>
+			<td width="400">
+				<h1><?
+				print "$jour[$day] ";
+				print Date(d)." ";
+				print "$mois[$month] ";
+				print Date(Y);?>
+				</h1>
+			</td>
 			<td width="125"><h2><img src="images/point.png"/>&nbsp;<a href="tableaubord.php">ZONE JEUX</a></h2></td>
+			<td width="125"><h2><img src="images/point.png"/>&nbsp;<a href="http://mobile.capakaspa.info">MOBILE</a></h2></td>
 			<td width="125"><h2><img src="images/point.png"/>&nbsp;<a href="http://forum.capakaspa.info">FORUM</a></h2></td>
 			<td width="125"><h2><img src="images/point.png"/>&nbsp;<a href="http://blog.capakaspa.info">BLOG</a></h2></td>
-			<td width="125"><h2><img src="images/point.png"/>&nbsp;<a href="http://www.facebook.com/capakaspa" target="_blank">FACEBOOK</a></h2></td>
+			
 		</tr>
     </table>
   </div>
   
 	<div id="progressbar">
-    
-    <table width="100%" cellpadding="2" cellspacing="0">
-	   <tr>
-    	<td align="center" width="20">
-        	<img src="images/puce.gif"/>
-        </td>
-    	<td width="400">
-        	<? echo($barre_progression) ?>
-        </td>
-        <td align="right" width="490">
-        
-        	<form action="http://www.capakaspa.info/recherche.php" id="cse-search-box">
-			  <div>
-			    <input type="hidden" name="cx" value="partner-pub-8069368543432674:5914611902" />
-			    <input type="hidden" name="cof" value="FORID:10" />
-			    <input type="hidden" name="ie" value="UTF-8" />
-			    <input type="text" name="q" size="31" />
-			    <input type="submit" name="sa" value="Rechercher" />
-			  </div>
-			</form>
-			
-			<script type="text/javascript" src="http://www.google.fr/coop/cse/brand?form=cse-search-box&amp;lang=fr"></script>
-
-    
-    	</td>
-	</tr>
-	</table>
-  </div>
+		<img src="images/puce.gif"/>  <? echo($barre_progression) ?>
+	</div>
   
   <div id="contentxlarge">
     <center>
