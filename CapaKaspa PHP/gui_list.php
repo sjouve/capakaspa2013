a0eb0e6c1e42252c2924732cd9a2e7b425d06067
Sebastien JOUVE <sjouve@gmail.com>
2011-12-03 17:01:03 +0100

    Evolutions v4.5

---
+++ CapaKaspa PHP/gui_list.php
@@ -0,0 +1,59 @@
+<?
+
+/* Affichage navigation pagination liste
+ * $pge = numéro de la page courante
+ * $limit = nombre de résultats par page
+ * $nb_tot = nombre de résultats
+ * $nbpages = nombre de pages
+ */
+function displayPageNav($pge, $limit, $nb_tot, $nbpages)
+{
+	echo("<div id='navliste'>");
+	// Affichage de la première page si nécessaire (si nb total de pages supérieur à 5)
+	if($nbpages > 1 and $pge > 0)
+		echo("<div class='bouton'><a href='javascript:loadPage(0)'><img src='/images/bt_paginateur_premier'/></a></div> ");
+				
+	// AFFICHAGE DU LIEN PRECEDENT SI BESOIN EST (LA PREMIERE PAGES EST 0)
+	if ($pge > 0)
+	{
+		$precedent = $pge - 1;
+		echo("<div class='bouton'><a href='javascript:loadPage(".$precedent.")'><img src='/images/bt_paginateur_precedent'/></a></div> ");
+	}
+
+	echo("<div class='pages'>");
+	// AFFICHAGE DES NUMEROS DE PAGE
+	$i=0;
+	$j=1;
+	if($nb_tot > $limit)
+	{
+		while($i < $nbpages)
+		{ //  Pour limiter l'affichage du nombre de pages restantes
+			if ($i > $pge-5 and $i < $pge+5)
+			{
+				if($i != $pge)
+					echo("<a href='javascript:loadPage(".$i.")'>".$j."</a> ");
+				else 
+					echo($j." "); // Page courante
+			}
+			$i++;
+			$j++;
+		}
+	}
+	echo("</div>");
+			
+	// AFFICHAGE DU LIEN SUIVANT SI BESOIN EST
+	if($pge < $nbpages-1)
+	{
+		$suivant = $pge+1;
+		echo("<div class='bouton'><a href='javascript:loadPage(".$suivant.")'><img src='/images/bt_paginateur_suivant'/></a></div> ");
+	}	
+	// Affichage de la dernière page si nécessaire
+	if($nbpages > 1 and $pge < $nbpages-1)
+	{
+		$fin = $nbpages-1;
+		echo("<div class='bouton'><a href='javascript:loadPage(".$fin.")'><img src='/images/bt_paginateur_dernier'/></a></div> ");
+	} 
+	echo("<div class='pages'> (".$nbpages." pages - ".$nb_tot." résultats)</div>");
+	echo("</div>");
+}
+?>
---
+++ CapaKaspa PHP/mobilecheck.php
@@ -0,0 +1,9 @@
+<?	require_once('class.detect_device.php');
+	$actual_device = new detect_device();
+	if (isset($_GET[ 'prevent_redirection' ])) {
+    	$actual_device->prevent_redirection();
+	}
+	$actual_device->mobileredirect = "http://mobile.capakaspa.info";
+	$actual_device->desktopredirect = false;
+	$actual_device->redirect();
+?>
