feb540f3afe9110db24eaadfac44d0f7c6ecd635
Sebastien JOUVE <sjouve@gmail.com>
2011-12-06 08:18:01 +0100

    Mise à jour pied de page

--- CapaKaspa PHP/page_footer.php
+++ CapaKaspa PHP/page_footer.php
@@ -1,5 +1,6 @@
   	<div id="footer">
-		CapaKaspa v4.5 - 12/2011 - Conception : <a href="http://www.netassistant.fr" target="_blank">NetAssistant.fr</a> - Contactez l'administrateur : capakaspa@capakaspa.info
+		CapaKaspa v4.5.1 - 12/2011 - Conception et réalisation : Sébastien JOUVE - 
+		Signaler un problème, faire une suggestion : capakaspa@capakaspa.info<br/><br/>
 	</div>
   
 </div>
