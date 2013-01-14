ffca7b8ada31908b344f92bc0258b5760dc7dc6a
sjouve <sjouve@gmail.com>
2013-01-14 13:17:03 +0100

    Création page messagerie

---
+++ CapaKaspa PHP/message.php
@@ -0,0 +1,44 @@
+<?
+	session_start();
+
+	/* load settings */
+	if (!isset($_CONFIG))
+		require 'include/config.php';
+
+	require 'bwc/bwc_common.php';
+	
+	/* connect to database */
+	require 'include/connectdb.php';
+
+	$errMsg = "";
+
+	/* check session status */
+	require 'include/sessioncheck.php';
+	
+	$titre_page = _("Message");
+	$desc_page = _("Message");
+    require 'include/page_header.php';
+?>
+    <script type="text/javascript">
+
+		
+		
+	</script>
+<?
+    require 'include/page_body_no_menu.php';
+?>
+	<div id="contentlarge">
+    	<div class="contentbody">
+	  	<?
+		if ($errMsg != "")
+			echo("<div class='error'>".$errMsg."</div>");
+		?>
+		
+		
+		
+    	</div>
+    </div>
+<?
+    require 'include/page_footer.php';
+    mysql_close();
+?>
