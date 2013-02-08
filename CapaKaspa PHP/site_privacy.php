1c68c124516a568fc57340a3cb01f4e02b067fe7
sjouve <sjouve@gmail.com>
2013-02-08 11:45:03 +0100

    Ajout page vie privée

---
+++ CapaKaspa PHP/site_privacy.php
@@ -0,0 +1,21 @@
+<?	
+$titre_page = _("Terms and policies");
+$desc_page = _("Terms and policies");
+require 'include/page_header.php';
+?>
+<script type="text/javascript">
+
+</script>
+<?
+require 'include/page_body_no_menu.php';
+?>
+<div id="contentlarge">
+    <div class="contentbody">
+		
+		
+		
+	</div>
+</div>
+<?
+require 'include/page_footer.php';
+?>
