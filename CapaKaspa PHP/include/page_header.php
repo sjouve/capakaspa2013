f060da013f5a318eb58a1dda6cf77e98a2a16e86
sjouve <sjouve@gmail.com>
2013-02-23 14:30:15 +0100

    Modification Keywords

--- CapaKaspa PHP/include/page_header.php
+++ CapaKaspa PHP/include/page_header.php
@@ -6,12 +6,12 @@
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 <meta http-equiv="content-language" content="fr" />
 <meta name="description" content="<? echo($desc_page)?>" />
-<meta name="keywords" content="échecs, différé, jouer, mat, jeu, échiquier, réflexion, stratégie, tactique, apprentissage, information, blog, forum" />
+<meta name="keywords" content="chess, échecs" />
 <meta name="robots" content="index, follow" />
 <meta name="generator" content="CapaKaspa - http://www.capakaspa.info" />
 <meta property="og:image" content="http://www.capakaspa.info/images/pawn_128.jpg"/>
 <link rel="stylesheet" href="css/capakaspa.css" type="text/css" />
 <link rel="shortcut icon" href="http://www.capakaspa.info/favicon.ico" />
-<link rel="alternate" type="application/rss+xml" title="CapaKaspa - Le forum " href="http://forum.capakaspa.info/topics_anywhere.php?mode=show&out=rss2.0&f=a&n=5&sfn=y&r=y&sr=y&a=y&so=d&b=non&lpb=0&lpd=0&af=p2xibKcgKKdmbqcpIKd0dKd8p2xwYacsIKdscHSnICincmVwpyk%3D" />
-<link rel="alternate" type="application/rss+xml" title="CapaKaspa - Le blog " href="http://blog.capakaspa.info/rss.php" />
+<link rel="alternate" type="application/rss+xml" title="CapaKaspa - Forum " href="http://forum.capakaspa.info/topics_anywhere.php?mode=show&out=rss2.0&f=a&n=5&sfn=y&r=y&sr=y&a=y&so=d&b=non&lpb=0&lpd=0&af=p2xibKcgKKdmbqcpIKd0dKd8p2xwYacsIKdscHSnICincmVwpyk%3D" />
+<link rel="alternate" type="application/rss+xml" title="CapaKaspa - Blog " href="http://blog.capakaspa.info/rss.php" />
 <link href="https://plus.google.com/114694270583726807082/" rel="publisher" />
