54f440df455ee85786aaede5084052ef75aff981
sjouve <sjouve@gmail.com>
2013-03-07 14:36:58 +0100

    Correction if ($lang=="fr")

--- CapaKaspa PHP/include/localization.php
+++ CapaKaspa PHP/include/localization.php
@@ -5,7 +5,7 @@ $lang = "en";
 // 2- If client language is french display french
 if (isSet($_SERVER['HTTP_ACCEPT_LANGUAGE']))
 	$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-if ($lang="fr") $locale = "fr_FR";
+if ($lang=="fr") $locale = "fr_FR";
 // 3- If user connected get preference language
 if (isSet($_SESSION["pref_language"]) && $_SESSION["playerID"]!=-1) $locale = $_SESSION["pref_language"];
 // 4- Finally language in URL
