51d4fd6aeeabbde1a89510234b43155f8dc567c1
sjouve <sjouve@gmail.com>
2014-09-30 23:31:32 +0200

    Nouveau code de suivi Analytics

--- CapaKaspa PHP/include/page_footer.php
+++ CapaKaspa PHP/include/page_footer.php
@@ -7,10 +7,15 @@
 	</div>  
 </div>
 
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
-<script type="text/javascript">
-_uacct = "UA-2529972-2";
-urchinTracker();
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-2529972-2', 'auto');
+  ga('send', 'pageview');
+
 </script>
 
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js">
