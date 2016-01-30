6dacca89ed0bb87ad7068bdcae419912ad05ff75
sjouve <sjouve@gmail.com>
2016-01-30 19:24:32 +0100

    Effet couleur sur clique menu

--- CapaKaspa PHP/mobile/include/page_body.php
+++ CapaKaspa PHP/mobile/include/page_body.php
@@ -11,6 +11,11 @@
   })();
 
 </script>
+<script type="text/javascript">
+function clickMenu(id) {
+	  document.getElementById(id).style.backgroundColor = "#6D6994";
+	}
+</script>
 </head>
 <body <?echo(isset($attribut_body)?$attribut_body:"")?>>
 <?
@@ -52,27 +57,27 @@ if (isset($_SESSION['playerID']) && $_SESSION['playerID']!=-1)
 		<table width="100%" cellpadding="0" cellspacing="0">
 		<tr>
 			<? if ($activeMenu == 10) {?>
-			<td><div class="ongletenable"><? echo _("Games"); if ($nbTurns > 0) echo(" <span class='newplayer' title='"._("Moves to play")."'>".$nbTurns."</span>");?></div></td>
+			<td><div id="menugames" class="ongletenable"><? echo _("Games"); if ($nbTurns > 0) echo(" <span class='newplayer' title='"._("Moves to play")."'>".$nbTurns."</span>");?></div></td>
 			<? } else {?>
-			<td><div class="ongletdisable" onclick="location.href='game_in_progress.php'"><a href="game_in_progress.php"><? echo _("Games"); if ($nbTurns > 0) echo(" <span class='newplayer' title='"._("Moves to play")."'>".$nbTurns."</span>");?></a></div></td>
+			<td><div id="menugames" class="ongletdisable" onclick="clickMenu(this.id);location.href='game_in_progress.php'"><? echo _("Games"); if ($nbTurns > 0) echo(" <span class='newplayer' title='"._("Moves to play")."'>".$nbTurns."</span>");?></div></td>
 			<? }?>
 			
 			<? if ($activeMenu == 20) {?>
-			<td><div class="ongletenable"><? echo _("Tournaments");?></div></td>
+			<td><div id="menutourn" class="ongletenable"><? echo _("Tournaments");?></div></td>
 			<? } else {?>
-			<td><div class="ongletdisable" onclick="location.href='tournament_list.php'"><a href="tournament_list.php"><? echo _("Tournaments");?></a></div></td>
+			<td><div id="menutourn" class="ongletdisable" onclick="clickMenu(this.id);location.href='tournament_list.php'"><? echo _("Tournaments");?></div></td>
 			<? }?>
 			
 			<? if ($activeMenu == 30) {?>
-			<td><div class="ongletenable"><? echo _("News"); if ($nbUnreadNews > 0) echo(" <span class='newplayer' title='"._("Unread news")."'>".$nbUnreadNews."</span>");?></div></td>
+			<td><div id="menunews" class="ongletenable"><? echo _("News"); if ($nbUnreadNews > 0) echo(" <span class='newplayer' title='"._("Unread news")."'>".$nbUnreadNews."</span>");?></div></td>
 			<? } else {?>
-			<td><div class="ongletdisable" onclick="location.href='activity.php'"><a href="activity.php"><? echo _("News"); if ($nbUnreadNews > 0) echo(" <span class='newplayer' title='"._("Unread news")."'>".$nbUnreadNews."</span>");?></a></div></td>
+			<td><div id="menunews" class="ongletdisable" onclick="clickMenu(this.id);location.href='activity.php'"><? echo _("News"); if ($nbUnreadNews > 0) echo(" <span class='newplayer' title='"._("Unread news")."'>".$nbUnreadNews."</span>");?></div></td>
 			<? }?>
 			
 			<? if ($activeMenu == 40) {?>
-			<td><div class="ongletenable"><? echo _("Players"); echo(" <span class='newplayer' title='"._("Online players")."'>".getNbOnlinePlayers()."</span>");?></div></td>
+			<td><div id="menuplay" class="ongletenable"><? echo _("Players"); echo(" <span class='newplayer' title='"._("Online players")."'>".getNbOnlinePlayers()."</span>");?></div></td>
 			<? } else {?>
-			<td><div class="ongletdisable" onclick="location.href='player_search.php'"><a href="player_search.php"><? echo _("Players"); echo(" <span class='newplayer' title='"._("Online players")."'>".getNbOnlinePlayers()."</span>");?></a></div></td>	
+			<td><div id="menuplay" class="ongletdisable" onclick="clickMenu(this.id);location.href='player_search.php'"><? echo _("Players"); echo(" <span class='newplayer' title='"._("Online players")."'>".getNbOnlinePlayers()."</span>");?></div></td>	
 			<? }?>
 		</tr>
 		</table>
